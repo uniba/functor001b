68a8b8283cb501bdbf4e1da9bc915c2a227c03d6
Haruma Kikuchi <haruma@uniba.jp>
2025-12-28 15:16:59 +0900

    Use three.js renderer directly

--- src/testvideo.js
+++ src/testvideo.js
@@ -3,44 +3,62 @@ AFRAME.registerComponent( "functor-videotest", {
   init() {
     console.log( "hi i am the videotest" );
 
-    window.addEventListener( 'arjs-video-loaded', function ( event ) {
+    window.addEventListener( 'arjs-video-loaded', ( event ) => {
 
-      const videoFromArjs = document.getElementById( "videoFromArjs" );
-      const videoFromAframe = document.getElementById( "videoFromAframe" );
-      const videoMerged = document.getElementById( "videoMerged" );
+      this.videoFromArjs = document.getElementById( "videoFromArjs" );
+      this.videoFromAframe = document.getElementById( "videoFromAframe" );
+      this.videoMerged = document.getElementById( "videoMerged" );
 
-      const arjsSource = document.getElementById( 'arjs-video' );
-      const aframeDom = AFRAME.scenes[0].renderer.domElement;
-      const aframeSource = document.getElementById( 'aframe-video' );
+      this.arjsSource = document.getElementById( 'arjs-video' );
+      this.aframeDom = this.el.sceneEl.renderer.domElement;
+      this.aframeSource = document.getElementById( 'aframe-video' );
 
-      console.dir( aframeSource );
+      this.arjsCanvas = document.createElement( "canvas" );
+      this.arjsCanvasContext = this.arjsCanvas.getContext( '2d', { willReadFrequently: true } );
+      this.aframeCanvas = document.createElement( "canvas" );
+      this.aframeCanvasContext = this.aframeCanvas.getContext( '2d', { willReadFrequently: true } );
+      this.mergedCanvas = document.createElement( "canvas" );
+      this.mergedCanvasContext = this.mergedCanvas.getContext( '2d', { willReadFrequently: true } );
 
-      const arjsCanvas = document.createElement( "canvas" );
-      const arjsCanvasContext = arjsCanvas.getContext( '2d' );
-      const aframeCanvas = document.createElement( "canvas" );
-      const aframeCanvasContext = aframeCanvas.getContext( '2d' );
-      const mergedCanvas = document.createElement( "canvas" );
-      const mergedCanvasContext = mergedCanvas.getContext( '2d' );
+      this.arjsCanvas.width = 100;//arjsSource.videoWidth;
+      this.arjsCanvas.height = 100;//arjsSource.videoWidth;
+      this.aframeCanvas.width = 100;//aframeSource.width;
+      this.aframeCanvas.height = 100;//aframeSource.height;
+      this.mergedCanvas.width = 100;//arjsSource.videoWidth;
+      this.mergedCanvas.height = 100;//arjsSource.videoHeight;
 
-      arjsCanvas.width = 100;//arjsSource.videoWidth;
-      arjsCanvas.height = 100;//arjsSource.videoWidth;
-      aframeCanvas.width = 100;//aframeSource.width;
-      aframeCanvas.height = 100;//aframeSource.height;
-      mergedCanvas.width = 100;//arjsSource.videoWidth;
-      mergedCanvas.height = 100;//arjsSource.videoHeight;
+      this.videoFromArjs.srcObject = this.arjsCanvas.captureStream( 25 );
+      this.videoFromAframe.srcObject = this.aframeCanvas.captureStream( 25 );
+      this.videoMerged.srcObject = this.mergedCanvas.captureStream( 25 );
+      this.aframeSource.srcObject = this.aframeDom.captureStream( 25 );
 
-      setInterval( () => {
-        arjsCanvasContext.drawImage( arjsSource, 0, 0, 100, 100 );
-        aframeCanvasContext.drawImage( aframeSource, 0, 0, 100, 100 );
-        mergedCanvasContext.drawImage( arjsSource, 0, 0, mergedCanvas.width, mergedCanvas.height );
-        mergedCanvasContext.drawImage( aframeSource, 0, 0, 100, 100 );
-      }, 1000 / 25 );
-
-      videoFromArjs.srcObject = arjsCanvas.captureStream( 25 );
-      videoFromAframe.srcObject = aframeCanvas.captureStream( 25 );
-      videoMerged.srcObject = mergedCanvas.captureStream( 25 );
-      aframeSource.srcObject = aframeDom.captureStream( 25 );
+      this.rendererHacked = false;
+      this.ready = true;
     } );
+  },
 
+  tick() {
+    const renderer = this.el.sceneEl.renderer;
+
+    if ( renderer && !this.rendererHacked ) {
+      const originalRender = renderer.render;
+      const self = this;
+
+      renderer.render = function ( scene, camera ) {
+        originalRender.call( this, scene, camera );
+
+        if ( self.ready ) {
+          self.aframeCanvasContext.drawImage( this.domElement, 0, 0, 100, 100 );
+          self.mergedCanvasContext.drawImage( self.arjsSource, 0, 0, 100, 100 );
+          self.mergedCanvasContext.drawImage( this.domElement, 0, 0, 100, 100 );
+        }
+      };
+
+      this.rendererHacked = true;
+    }
+
+    if ( this.ready ) {
+      this.arjsCanvasContext.drawImage( this.arjsSource, 0, 0, 100, 100 );
+    }
   }
 } );
