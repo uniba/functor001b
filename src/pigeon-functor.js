efcc0699918cbc8efabb43c39b712dce82f191a9
Haruma Kikuchi <haruma@uniba.jp>
2025-12-29 11:50:18 +0900

    Use Lattepanda

--- src/pigeon-functor.js
+++ src/pigeon-functor.js
@@ -2,7 +2,7 @@ import { Pigeon } from "./Pigeon.js";
 
 AFRAME.registerComponent( "pigeon-functor", {
   init() {
-    this.pigeon = new Pigeon( "wss://vmb207.circuitlab.team:3001/pigeon/", 'functor' );
+    this.pigeon = new Pigeon( "wss://192.168.128.37:3001/pigeon/", 'functor' );
 
     this.x = 0;
     this.y = 0;
--- src/pigeon-receiver.js
+++ src/pigeon-receiver.js
@@ -2,7 +2,7 @@ import { Pigeon } from "./Pigeon.js";
 
 AFRAME.registerComponent( "pigeon-receiver", {
   init() {
-    this.pigeon = new Pigeon( "wss://vmb207.circuitlab.team:3001/pigeon/", 'functor' );
+    this.pigeon = new Pigeon( "wss://192.168.128.37:3001/pigeon/", 'functor' );
 
     document.addEventListener( "ipad", ( e ) => {
       const quaternion = new THREE.Quaternion( e.detail.x, e.detail.y, e.detail.z, e.detail.w );
